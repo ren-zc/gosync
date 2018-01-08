9b11fa1c0f9ffe207555ec2719ee472474432e91
rzcTree <rluckzflyc@gmail.com>
2018-01-08 18:42:23 +0800

    in progress

--- test/maintest.go
+++ test/maintest.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// gosync.RandId()
 	// fmt.Println(gosync.RandId())
-	str, err := Zipfiles("/data/mygo/src/github.com/jacenr/filediff")
+	str, err := gosync.Zipfiles("/data/mygo/src/github.com/jacenr/filediff")
 	fmt.Println(str)
 	fmt.Println(err)
 }
