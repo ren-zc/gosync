d5b2640529d408e12082abb49b22133913fc3d01
rzcTree <rluckzflyc@gmail.com>
2018-01-15 14:17:18 +0800

    update sync

--- test/maintest.go
+++ test/maintest.go
@@ -13,11 +13,11 @@ func main() {
 	// fmt.Println(err)
 	// s, _ := gosync.Md5OfAFile("gosync架构")
 	// fmt.Println(s)
-	s1, m1, e1 := gosync.Traverse("/data/mygo/src/github.com/jacenr/filediff", true)
+	s1, e1 := gosync.Traverse("/data/mygo/src/github.com/jacenr/filediff")
 	for _, v := range s1 {
 		fmt.Println(v)
 	}
-	// fmt.Println(s1)
-	fmt.Println(m1)
+	fmt.Println(s1)
+	// fmt.Println(m1)
 	fmt.Println(e1)
 }
