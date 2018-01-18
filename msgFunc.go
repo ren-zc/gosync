13346672d62f19209d2b5037102d8c321c9cf0d3
rzcTree <rluckzflyc@gmail.com>
2018-01-18 13:32:57 +0800

    for test

--- msgFunc.go
+++ msgFunc.go
@@ -117,6 +117,7 @@ func hdFileMd5List(mg *Message, cnRd *bufio.Reader, cnWt *bufio.Writer, dec *gob
 	ret.MgType = "diffOfFilesMd5List"
 	ret.MgString = transFilesMd5 // for check, reserved
 	// encerr:= enc.Encode(ret) 暂时不考虑这种情况, 需配合源主机的超时机制
+	fmt.Println(ret)
 	enc.Encode(ret)
 
 	// do symbol link change
