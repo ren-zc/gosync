b30b2bba899647676eb18e6842de8c8da1dbc1ce
rzcTree <rluckzflyc@gmail.com>
2018-01-23 10:45:06 +0800

    50% add tastid

--- transUnit.go
+++ transUnit.go
@@ -34,6 +34,7 @@ func getTransUnit(zip bool, hostNum int, diffCh chan diffInfo, retCh chan hostRe
 			putRetCh(di.hostIP, nil, retCh)
 			continue
 		}
+		lg.Println(di)
 		tu, ok := tus[di.md5s]
 		if ok {
 			tu.hosts = append(tu.hosts, di.hostIP)
