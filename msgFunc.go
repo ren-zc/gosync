948a3b54930bbb6c6809d8616f814b15cee24fff
rzcTree <rluckzflyc@gmail.com>
2018-01-23 17:13:12 +0800

    50% add tastid

--- msgFunc.go
+++ msgFunc.go
@@ -83,7 +83,8 @@ func hdTask(mg *Message, gbc *gobConn) {
 		sort.Strings(fileMd5List)
 		listMd5 := Md5OfASlice(fileMd5List)
 		TravHosts(targets, fileMd5List, md5s(listMd5), mg, diffCh, retCh, taskID)
-		time.Sleep(2 * time.Minute)
+
+		// time.Sleep(2 * time.Minute)
 
 		// get transUnits
 		var tus = make(map[md5s]transUnit)
@@ -118,7 +119,6 @@ func hdTask(mg *Message, gbc *gobConn) {
 	default:
 		writeErrorMg(mg, "error, not a recognizable MgName.", gbc)
 	}
-
 }
 
 func hdFile(mg *Message, gbc *gobConn) {
