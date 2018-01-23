4b5c1079689ae702024ecb87180fd76a7512e4b7
rzcTree <rluckzflyc@gmail.com>
2018-01-23 10:16:35 +0800

    50% add tastid

--- msgFunc.go
+++ msgFunc.go
@@ -57,6 +57,7 @@ func hdTask(mg *Message, gbc *gobConn, conn net.Conn) {
 			if t.ask(taskID) {
 				break
 			}
+			fmt.Println("hdTask")
 			time.Sleep(1 * time.Second)
 		}
 		diffCh := make(chan diffInfo)
