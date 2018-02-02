4013a32db7a7ccf2efe37286457a5dca3dd3e122
rzcTree <rluckzflyc@gmail.com>
2018-02-02 16:31:01 +0800

    fix bug

--- debug.go
+++ debug.go
@@ -17,6 +17,14 @@ func DubugInfor(a ...interface{}) {
 			file = "???"
 			line = 0
 		}
+		short := file
+		for i := len(file) - 1; i > 0; i-- {
+			if file[i] == '/' {
+				short = file[i+1:]
+				break
+			}
+		}
+		file = short
 		fmt.Printf("* %s:%d: ", file, line)
 		for _, v := range a {
 			fmt.Printf("%v", v)
