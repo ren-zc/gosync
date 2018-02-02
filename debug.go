b1918f94c473c510620a486d55f454490aed594d
rzcTree <rluckzflyc@gmail.com>
2018-02-02 19:01:45 +0800

    change log to DebugInfo

--- debug.go
+++ debug.go
@@ -10,7 +10,27 @@ var DebugFlag bool
 // 用于debug时输出文件号及行号, Dubug-->Debug, 错别字
 func DubugInfor(a ...interface{}) {
 	if DebugFlag {
-		PrintInfor(a)
+		var file string
+		var line int
+		var ok bool
+		_, file, line, ok = runtime.Caller(1)
+		if !ok {
+			file = "???"
+			line = 0
+		}
+		short := file
+		for i := len(file) - 1; i > 0; i-- {
+			if file[i] == '/' {
+				short = file[i+1:]
+				break
+			}
+		}
+		file = short
+		fmt.Printf("* %s:%d: ", file, line)
+		for _, v := range a {
+			fmt.Printf("%v", v)
+		}
+		fmt.Println()
 	}
 }
 
