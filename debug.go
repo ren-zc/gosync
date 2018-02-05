96a1402d80ebf4b0325f9109d44fbfe32e404516
rzcTree <rluckzflyc@gmail.com>
2018-02-05 10:22:26 +0800

    debug func

--- debug.go
+++ debug.go
@@ -10,36 +10,20 @@ var DebugFlag bool
 // 用于debug时输出文件号及行号, Dubug-->Debug, 错别字
 func DubugInfor(a ...interface{}) {
 	if DebugFlag {
-		var file string
-		var line int
-		var ok bool
-		_, file, line, ok = runtime.Caller(1)
-		if !ok {
-			file = "???"
-			line = 0
-		}
-		short := file
-		for i := len(file) - 1; i > 0; i-- {
-			if file[i] == '/' {
-				short = file[i+1:]
-				break
-			}
-		}
-		file = short
-		fmt.Printf("* %s:%d: ", file, line)
-		for _, v := range a {
-			fmt.Printf("%v", v)
-		}
-		fmt.Println()
+		getInfor(a, 2)
 	}
 }
 
 // 输出信息
 func PrintInfor(a ...interface{}) {
+	getInfor(a, 2)
+}
+
+func getInfor(a ...interface{}, depth int) {
 	var file string
 	var line int
 	var ok bool
-	_, file, line, ok = runtime.Caller(1)
+	_, file, line, ok = runtime.Caller(depth)
 	if !ok {
 		file = "???"
 		line = 0
