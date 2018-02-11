4f4fd7c4dd9b37a818ad49d942de31212bca59cd
rzcTree <rluckzflyc@gmail.com>
2018-02-11 16:32:58 +0800

    final test

--- traversFile.go
+++ traversFile.go
@@ -60,7 +60,7 @@ func Traverse(path string) ([]string, error) {
 	// DubugInfor(md5List)
 	for i, v := range md5List {
 		DubugInfor(v)
-		if v == " " || v == ".,,Directory" {
+		if v == "" || v == ".,,Directory" {
 			continue
 		}
 		md5List = md5List[i:]
